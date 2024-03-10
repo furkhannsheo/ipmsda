386c96c0ef68e2a6de83a315da885fa4b66a724a
furkhannsheo <dani@smkkrian1.sch.id>
2024-03-10 18:04:11 +0700

    finally2

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'http://localhost/company/';
+	public $baseURL = 'http://localhost/ipmsda/';
 
 	/**
 	 * --------------------------------------------------------------------------
--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -8,18 +8,6 @@ $menu_berita  = $menu->berita();
 $menu_profil  = $menu->profil();
 $menu_layanan  = $menu->layanan();
 ?>
-<!-- ======= Top Bar ======= -->
-  <div id="topbar" class="d-flex align-items-center fixed-top">
-    <div class="container d-flex align-items-center justify-content-center justify-content-md-between">
-      <div class="align-items-center d-none d-md-flex">
-        <i class="fa fa-home"></i> <?php echo tagline(); ?>
-      </div>
-      <div class="d-flex align-items-center">
-        <i class="bi bi-phone"></i> <?php echo telepon() ?>
-      </div>
-    </div>
-  </div>
-
   <!-- ======= Header ======= -->
   <header id="header" class="fixed-top">
     <div class="container d-flex align-items-center">
